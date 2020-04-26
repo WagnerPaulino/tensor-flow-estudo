23b8f0753c74cb384f8f5e6301b2bd2ceae5bb7b
Wagner Paulino <wagnersantos9778@gmail.com>
2020-04-26 20:34:03 -0300

    classificação com texto pre processado - exemplo tensorflow

--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,4 @@
-docker build --build-arg RUN_FILE=primeira_rede_neural_tensor_flow.py -t tensor-flow-estudo .
+docker build --build-arg RUN_FILE=classificacao_texto_preprocessado.py -t tensor-flow-estudo .
 docker rm -f tensor
 docker run --name tensor tensor-flow-estudo
 docker logs -f tensor
