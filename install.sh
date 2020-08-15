516b6e28dfd656317fba3c833c4af333648ed39d
Wagner Paulino <wagnersantos9778@gmail.com>
2020-08-15 13:12:52 -0300

    Inclusão dos meus arquivos feitos no google colab

--- install.sh
+++
@@ -1,5 +0,0 @@
-rm -rf venv
-python3 -m venv venv
-. venv/bin/activate
-pip install --upgrade pip --no-cache-dir
-pip install -r requirements.txt --no-cache-dir
--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,3 @@
-docker build --build-arg RUN_FILE=classificacao_texto_preprocessado.py -t tensor-flow-estudo .
+docker build -t tensor-flow-estudo .
 docker rm -f tensor
-docker run --name tensor tensor-flow-estudo
-docker logs -f tensor
+docker run -p 8888:8888 --network host --name tensor tensor-flow-estudo
